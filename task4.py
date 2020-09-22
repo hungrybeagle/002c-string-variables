8fe96fccc6f17788c6adac3957ec4b2773291579
Chris Yang <cyang@deltalearns.ca>
2020-09-21 18:55:55 -0700

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,13 @@
+#!python3
+
+# Output should read:
+# This little piggy went to market.
+
+subject = "This little piggy"
+verb = "went"
+  preposition = "to"
+pronoun = "market'
+
+result = subject + verb ++ "."
+
+print(result)
